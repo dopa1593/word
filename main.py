839edeb684c74d34266f4f0fc0f00127e19a5592
dopa1593 <wkdqkfwkd123456@naver.com>
2022-12-13 08:49:51 +0900

    듣기 및 객관식 기능 추가

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import csv,os,random
+from gtts import gTTS
+from playsound import playsound
 
 def add_csv(name, b, n):#csv 추가
   
@@ -81,7 +83,13 @@ def random_word():#origin에서 랜덤하게 문제 풀이
 
   for i,a in enumerate(data): 
     q = "{:d} {:s} : ".format(i+1,a[1])
-    answer = str(input(q))
+    answer = input(q)
+
+    while answer == '듣기':
+        hear(a[0])
+        answer = input(q)
+        
+
     if answer == '종료':
       break
     elif answer == a[0]:
@@ -112,7 +120,12 @@ def remind():#틀린문제 풀이, 맞추면 틀린문제 삭제
 
   for i,a in enumerate(data): 
     q = "{:d} {:s} : ".format(i,a[1])
-    answer = str(input(q))
+    answer = input(q)
+
+    while answer == '듣기':
+        hear(a[0])
+        answer = input(q)
+        
     if answer == '종료':
       break
     elif answer == a[0]:
@@ -133,9 +146,80 @@ def clear():
   print("정보가 초기화 되었습니다")
   print("-" * 90)
 
+def hear(a):#단어 재생
+    try:
+      word = a
+      tts = gTTS(text = word, lang = 'en-us')
+      tts.save(r'C:\Users\mit\Desktop\python_project\word\hear.mp3')
+      playsound('hear.mp3')
+      os.remove(r'C:\Users\mit\Desktop\python_project\word\hear.mp3')
+      return 
+    except:
+      print('\n')
+      return
+
+def clear():#틀린문제 csv 초기화
+  data = []
+  insert_csv('wrong_answer',data)
+  print("정보가 초기화 되었습니다")
+  print("-" * 90)
+    
+def word_4(a):
+  data = open_csv('origin')
+  random.shuffle(data)
+  result = []
+
+  if a in data[:3]:
+    result.append(data[-1][1])
+    result.append(data[-2][1])
+    result.append(data[-3][1])
+  else:
+    result.append(data[0][1])
+    result.append(data[1][1])
+    result.append(data[2][1])
+  
+  result.append(a)
+
+  random.shuffle(result)
+
+  return result  
+
+def choice_word():
+  count = 0
+  wrong_answer = []
+
+  data = open_csv('origin')
+  random.shuffle(data)
+
+  print("-" * 90)
+
+  for i,a in enumerate(data): 
+    q = "{:d} {:s} : ".format(i+1,a[0])
+    q_word = word_4(a[1])
+    q =  "1)" + q_word[0] + "\n2)" + q_word[1] + "\n3)" + q_word[2] + "\n4)" + q_word[3] + "\n\n" + q
+    
+    answer = input(q)
+
+    while answer == '듣기':
+        hear(a[0])
+        answer = input(q)
+      
+    if answer == '종료':
+      break
+    elif q_word[int(answer)-1] == a[1]:
+      print("정답!\n")
+    else:
+      print("오답...\n")
+      wrong_answer.append(a)
+      count += 1
+
+  print("무작위 암기가 종료되었습니다.")
+  print("-" * 90)
+
+  add_csv('wrong_answer',wrong_answer,count)
+  return
 
 while True:
-
   a = input("입력 : ")
   if a == '종료':
      break
@@ -147,13 +231,26 @@ while True:
   elif a == '틀린문제연습':
     remind()
   elif a == '초기화':
-    a = input("정말 초기화 하시겠습니까? (Y/N)")
+    a = input("정말 초기화 하시겠습니까? (Y/N) : ")
     if a == 'y' or 'Y':
       clear()
     else:
       print("취소되었습니다")
+  elif a == '객관식연습':
+      choice_word()
   else:
     print("존재하기 않는 명령어")
 
+# a = open_csv('wrong_answer')
+# b = []
+# for i in a[:5]:
+#   i.reverse()
+#   print(i)
+#   b.append(i)
+# insert_csv('wrong_answer', b)
+
+# a = open_csv('wrong_answer')
+
+# print(a)
 
 
